386d0b8d1b83932e308bac282ee4154abc22deb5
Alejandro Mery <amery@geeks.cl>
2012-07-21 17:03:10 +0200

    generator: improve parsing error handling

--- src/lua/sancus/template/generator.lua
+++ src/lua/sancus/template/generator.lua
@@ -8,7 +8,8 @@ local lpeg = assert(require"lpeg")
 local P,R,S,V = lpeg.P, lpeg.R, lpeg.S, lpeg.V
 local C,Cc,Cg,Ct,Cp = lpeg.C, lpeg.Cc, lpeg.Cg, lpeg.Ct, lpeg.Cp
 
-local assert, setmetatable, type = assert, setmetatable, type
+local assert, setmetatable, tostring, type = assert, setmetatable, tostring, type
+local sformat = string.format
 
 local _M = {}
 
@@ -48,13 +49,59 @@ local function parser()
 end
 parser = parser()
 
-local function new(s)
-	assert(type(s) == "string")
+local function find_nl()
+	local nl = P"\n" + P"\r\n"
+	local eos = P(-1)
+
+	local line = (nl + ((1-nl)^1 * nl))*Cp()
+	return Ct(line^0)
+end
+find_nl = find_nl()
+
+local function parse(s)
+	assert(type(s) == "string", "argument must be an string")
+
+	if #s == 0 then
+		return {}
+	end
 
 	local t, p = parser:match(s)
 	if t and p == #s+1 then
 		return t
+	elseif p == nil then
+		p = 1
 	end
+
+	-- useful error message
+	local line, from, to = 1, 0, nil
+	t = find_nl:match(s)
+	for _, q in ipairs(t) do
+		if q <= p then
+			line = line + 1
+			from = q
+		else
+			to = q-2
+			break
+		end
+	end
+	local col = p - from
+	local line = tostring(line)
+
+	local s1 = s:sub(from, to)
+	local s2 = (" "):rep(#line+1)
+
+	if col > 0 then
+		s2 = s2 ..  s1:gsub("[^\t]", " "):sub(1,col)
+	end
+	s2 = s2 .. "^"
+
+	error(sformat("sancus.template.parse: invalid element at (%s,%d):\n%s:%s\n%s",
+		line, col+1, line, s1, s2), 2)
+end
+
+local function new(s)
+	local t = parse(s)
+	return t
 end
 
 setmetatable(_M, {
